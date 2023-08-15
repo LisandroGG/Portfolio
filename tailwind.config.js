83e0cf7b378f1c83889c4024940c07fd9b7e9dcb
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-08-15 17:52:31 -0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: 'jit',
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
       
