92459806bb474ef9d8ea79b98b26cd4ddd5dc32c
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-08-23 18:40:01 -0300

    about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
                 <span className="text-5xl font-bold text-sky-400 underline text-left pb-10">About me</span>
                 <div className="font-bold grid grid-cols-2">
                     <div>
-                        <h1>Hola, soy Lisandro Pereyra, un Desarrolador Full-Stack graduado en henry.</h1>
-                        <h1>Aunque me gusta más enfocarme en el front-end porque ademas de funcional, se tienen que ver bonitas las cosas para dar mejor impresion.</h1>
-                        <h1>Para que me conozcan un poco, desde pequeño me gustaba todo lo que tenia que ver con crear, imaginar, ser creativo, activo, productor, en si me gustaba todo lo que me dejaba crear cosas y demostrarle a la gente lo que se hacer, terminando mi trayecto de secundaria, decidi anotarme en Henry, donde estudie en el bootcamp de 800 horas teoricas, tambien pude hacer compañeros, y aprender muchas cosas valiosas a lo largo de mi camino en Henry.</h1>
-                        <h1>Lo que nos lleva a la actualidad de ahora, que estoy buscando empleo, estoy abiertamente a postulaciones, reuniones, preguntas, a lo que necesiten, por eso los invito a ver mis medios de Contacto</h1>
+                        <h1>Hola! Soy Lisandro y soy Desarrollador Fullstack, graduado en henry, donde he tenido una formacion intensiva de 800 horas teórico y practicas, he podido realizar proyectos reales y vivenciado ambos lados del desarrollo, tanto el Back como el Front aunque disfruto mucho del frontend.</h1>
+                        <h1>Desde pequeño he tenido mucha facilidad e interés por todos los ámbitos que me permitieran explorar mi creatividad, la tecnología me ha permitido utilizar mi creatividad al 100%, este gran interés por el mundo de la tecnología y del desarrollo me llevó a sumarme a Henry donde pude aprender no solo a programar, sino a trabajar en equipo, utilizar metodologias agiles, a ser tolerante a la frustracion entre otras habilidades.</h1>
+                        <h1>Hoy busco incorporarme a un equipo donde pudiera crecer profesionalmente poniendo a disposición todos mis recursos.</h1>
+                        <h1>Si te interesa mi perfil y crees que puedo ser un buen match no dudes en contactarme!</h1>
                     </div>
                     <div className='px-30'>
-                            <span className='bg-zinc-900 grid place-items-center rounded-full border-4 p-4 border-sky-500 scale-100 shadow-2xl shadow-sky-500 hover:(transition-all scale-105)'>
+                            <span className='bg-zinc-900 grid place-items-center rounded-full border-4 p-4 border-sky-500 shadow-2xl shadow-sky-500'>
                                 <img src={lisandro} width='220rem' className='rounded-xl'></img>
                             </span>
                     </div>
