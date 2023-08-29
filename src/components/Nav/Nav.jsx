e7aadd1df420866f3940ab954d8c90330e28edb4
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-08-29 12:34:35 -0300

    vista tablet hecha

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
 
     return(
         
-        <div className="flex justify-between text-white bg-zinc-900 border-b-4 border-sky-600 shadow-md shadow-sky-500 xl:(p-6 text-xl fixed w-full) md:(p-6 text-xl) sm:(p-6 )">
+        <div className="flex justify-between text-white bg-zinc-900 border-b-4 border-sky-600 shadow-md shadow-sky-500 xl:(p-6 text-xl fixed w-full) md:(p-6 text-xl) sm:(p-3)">
             <div className="flex">
                 <a href="#home">{icon}</a>
                 <ul className="flex text-white">
@@ -16,7 +16,7 @@ const Nav = () => {
             </div>
                 
             <div>
-                <a href='https://docs.google.com/document/d/1mB9tp6fL8DBOf53kFjkyF1fU_pattDWn55k00FQZBPI/edit?usp=sharing' target="_Blank" className='bg-sky-600 rounded-2xl p-2 hover:(bg-sky-700 shadow-md shadow-sky-800)'>Ver currículum</a>
+                <a href='https://docs.google.com/document/d/1mB9tp6fL8DBOf53kFjkyF1fU_pattDWn55k00FQZBPI/edit?usp=sharing' target="_Blank" className='bg-sky-600 rounded-2xl p-2 hover:(bg-sky-700 shadow-md shadow-sky-800) '>Ver currículum</a>
             </div>
         </div>
     )
